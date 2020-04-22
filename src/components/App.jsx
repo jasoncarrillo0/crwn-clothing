0ef6fccc6eb7bf751ba65d52ab9c5d05d8373255
Jason Carrillo <jasoncarrillo0@gmail.com>
2020-04-22 17:27:37 +0300

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link, Route, BrowserRouter as Router  } from 'react-router-dom';
+import HomePage from './HomePage';
+
+const App = () => {
+    return (
+        <HomePage/>
+    )
+}
+export default App;
